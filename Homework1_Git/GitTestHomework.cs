d13ec51050d5e040a63e24103b34ec5458c1cdd8
Eric Ray <erichamion@gmail.com>
2022-01-07 15:24:08 -0600

    Initial commit

---
+++ Homework1_Git/GitTestHomework.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Homework1_Git
+{
+    [TestClass]
+    public class GitTestHomework
+    {
+        [TestMethod]
+        public void GitWorking()
+        {
+            Assert.IsTrue(true); // just so testing passes
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+Claim Academy Git Homework
+
+Please submit your branch with the following format
+firstName_LastName_HomeworkName
+
+So for this git assignment my branch would be:
+cody_sevier_git
